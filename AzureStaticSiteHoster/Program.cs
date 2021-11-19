4e94d39b2e40375b984cb43c0f62e7cb042744c8
Reuben Swartz <rbn.swartz@gmail.com>
2021-11-18 19:11:34 -0500

    Initial application

---
+++ AzureStaticSiteHoster/Program.cs
@@ -0,0 +1,34 @@
+using Azure.Storage.Blobs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<BlobContainerClient>((services) =>
+{
+    var config = services.GetService<IConfiguration>();
+    var connectionStringConfig = config.GetValue<string>("AzureStorageConnectionString");
+    var storageContainerConfig = config.GetValue<string>("AzureStorageContainer");
+    return new BlobContainerClient(connectionStringConfig, storageContainerConfig);
+});
+
+var app = builder.Build();
+
+app.MapGet("{*url}", async (string? url, BlobContainerClient container) =>
+{
+    string path = url ?? "/";
+    if (path.EndsWith('/'))
+    {
+        path += "index.html";
+    }
+    var blob = container.GetBlobClient(path);
+    if (await blob.ExistsAsync())
+    {
+        var stream = new MemoryStream();
+        var propertiesTask = blob.GetPropertiesAsync();
+        await blob.DownloadToAsync(stream);
+        stream.Position = 0;
+        return Results.Stream(stream, contentType: (await propertiesTask).Value.ContentType);
+    }
+    return Results.NotFound("Not found");
+});
+
+app.Run();
